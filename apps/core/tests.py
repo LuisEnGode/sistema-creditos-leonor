fe8bcbec7d1a9933bc771e9546bc91c07966f2ec
LuisEnGode <luisengode@gmail.com>
2026-08-31 16:39:39 -0500

    feat: configurar base HTMX

--- apps/core/tests.py
+++ apps/core/tests.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+from django.urls import reverse
 
 
 class HealthApiTests(TestCase):
@@ -7,3 +8,24 @@ class HealthApiTests(TestCase):
 
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.json(), {"status": "ok"})
+
+
+class CoreViewTests(TestCase):
+    def test_home_loads(self):
+        response = self.client.get(reverse("home"))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "core/home.html")
+
+    def test_htmx_test_returns_partial(self):
+        response = self.client.get(reverse("htmx_test"))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(
+            response,
+            "core/partials/htmx_response.html",
+        )
+        self.assertContains(
+            response,
+            "HTMX está funcionando correctamente",
+        )
--- apps/core/views.py
+++ apps/core/views.py
@@ -1,1 +1,9 @@
-# Create your views here.
+from django.shortcuts import render
+
+
+def home(request):
+    return render(request, "core/home.html")
+
+
+def htmx_test(request):
+    return render(request, "core/partials/htmx_response.html")
